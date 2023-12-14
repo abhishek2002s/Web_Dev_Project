ba8bbd8b18fdf75c2aac7f62ae5a79ab2c3e68de
abhishek2002s <linkedin23356abhi@gmail.com>
2023-12-15 02:41:52 +0530

    SIDCUP FAMILY GOLF

--- Website_1/script.js
+++ Website_1/script.js
@@ -10,7 +10,19 @@ blr.style.left = dets.x - 250 + "px"
 blr.style.top = dets.y - 250 + "px"
 })
 
-
+var h4all = document.querySelectorAll("#nav h4");
+h4all.forEach(function (elem) {
+  elem.addEventListener("mouseenter", function () {
+    crsr.style.scale = 3;
+    crsr.style.border = "1px solid #fff";
+    crsr.style.backgroundColor = "transparent";
+  });
+  elem.addEventListener("mouseleave", function () {
+    crsr.style.scale = 1;
+    crsr.style.border = "0px solid #95C11E";
+    crsr.style.backgroundColor = "#95C11E";
+  });
+});
 
 gsap.to("#nav",{
     backgroundColor:"#000",
@@ -30,9 +42,74 @@ gsap.to("#main",{
     scrollTrigger:{
         trigger:"#main",
         scroller:"body",
-        markers:true,
+        // markers:true,
         start:"top -25%",
         end:"top -70%",
         scrub:2,
     },
 });
+
+gsap.from("#about-us img,#about-us-in", {
+    y: 90,
+    opacity: 0,
+    duration: 1,
+    scrollTrigger: {
+      trigger: "#about-us",
+      scroller: "body",
+      // markers:true,
+      start: "top 70%",
+      end: "top 65%",
+      scrub: 3,
+    },
+  });
+
+gsap.from(".card", {
+    scale:0.8,
+    duration: 1,
+    stagger:0.1,
+    scrollTrigger: {
+      trigger: ".card",
+      scroller: "body",
+    //   markers:true,
+      start: "top 70%",
+      end: "top 65%",
+      scrub: 3,
+    },
+  });
+
+  gsap.from("#colon1",{
+    y:-70,
+    x:70,
+    scrollTrigger:{
+        trigger:"#colon1",
+        scroller:"body",
+        // markers:true,
+        start:"top 50%",
+        end:"top 45%",
+        scrub:4,
+    },
+  });
+  gsap.from("#colon2",{
+    y:70,
+    x:-70,
+    scrollTrigger:{
+        trigger:"#colon1",
+        scroller:"body",
+        // markers:true,
+        start:"top 50%",
+        end:"top 45%",
+        scrub:4,
+    },
+  });
+
+  gsap.from("#page4 h1",{
+    y:30,
+    scrollTrigger:{
+        trigger:"#page4 h1",
+        scroller:"body",
+        // markers:true,
+        start:"top 75%",
+        end:"top 70%",
+        scrub:4,
+    },
+  })

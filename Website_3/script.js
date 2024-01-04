e86a2b25659ffc0d3e05de79757deaf943a57367
abhishek2002s <linkedin23356abhi@gmail.com>
2024-01-05 00:01:26 +0530

    Sundown studio clone

--- Website_3/script.js
+++ Website_3/script.js
@@ -4,6 +4,8 @@ const scroll = new LocomotiveScroll({
 });
 
 
+
+
 var elemc = document.querySelector("#elem-container")
 var fix = document.querySelector("#fixed-image")
 elemc.addEventListener("mouseenter",function(){
@@ -11,4 +13,13 @@ fix.style.display="block"
 })
 elemc.addEventListener("mouseleave",function(){
 fix.style.display="none"
+})
+
+var elems = document.querySelectorAll(".elem")
+elems.forEach(function(e){
+    e.addEventListener("mouseenter",function(){
+        var iamge = e.getAttribute("data-image")
+        fix.style.backgroundImage = `url(${iamge})`
+    })
+
 })

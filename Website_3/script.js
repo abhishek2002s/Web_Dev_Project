f3ca9c72d7a90117d09925dce99cced3f20cc071
abhishek2002s <linkedin23356abhi@gmail.com>
2024-01-04 23:42:48 +0530

    Sundown website

--- Website_3/script.js
+++ Website_3/script.js
@@ -2,3 +2,13 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+
+var elemc = document.querySelector("#elem-container")
+var fix = document.querySelector("#fixed-image")
+elemc.addEventListener("mouseenter",function(){
+fix.style.display="block"
+})
+elemc.addEventListener("mouseleave",function(){
+fix.style.display="none"
+})

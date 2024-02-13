79f3844339ea99af2b671386336828692d1e80b9
abhishek2002s <linkedin23356abhi@gmail.com>
2024-02-14 00:49:44 +0530

    Complete CLone

---
+++ Website_5/script.js
@@ -0,0 +1,38 @@
+Shery.mouseFollower();
+
+Shery.makeMagnet(".magnet,img");
+
+Shery.hoverWithMediaCircle(".h",{
+    videos: ["./images/0.mp4","./images/2.mp4","./images/3.mp4"],
+});
+
+
+gsap.to(".fleftelem", {
+    scrollTrigger: {
+      trigger: ".fimage",
+      pin: true,
+      start: "top top",
+      end: "bottom bottom",
+      endTrigger: ".last",
+      scrub: 1,
+    },
+    y: "-300%",
+    ease: Power1,
+  });
+  
+  const sections = document.querySelectorAll(".fleftelem");
+  Shery.imageEffect(".image", {
+      style: 1,
+      slideStyle: (setScroll) => {
+          sections.forEach(function (section, index) {
+              ScrollTrigger.create({
+                  trigger: section,
+                  start: "top top",
+                  scrub: 1,
+                  onUpdate: function (prog) {
+                      setScroll(prog.progress + index);
+                  },
+              });
+          });
+      },
+  });
